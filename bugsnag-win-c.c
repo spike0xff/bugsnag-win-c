3d1f010795d2bc7717487e932f5e770b6ed70747
Spike McLarty <spike.mclarty@gmail.com>
2016-11-21 17:27:06 -0500

    added example of a pretty minimal bugsnag JSON payload, and a note about working cURL command line.

--- bugsnag-win-c.c
+++ bugsnag-win-c.c
@@ -4,6 +4,9 @@
 #include "bugsnag-win-c.h"
 #include "string.h"
 
+// example working cURL command:
+// curl -H "Content-Type: application/json" --data-binary @minimal.json -v http://notify.bugsnag.com
+
 static char apiKey[64];
 
 void bugsnag_set_apiKey(const char* key)
